6dface57feeab77b5ddee501e79318668f96b399
ikhlashmulya <nurahmanmulya@gmail.com>
2025-06-21 13:27:46 +0700

    update layout

--- components/dzikir-layout.tsx
+++ components/dzikir-layout.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   ScrollView,
-  StyleSheet,
   Text,
   TouchableOpacity,
   View,
@@ -36,17 +35,27 @@ export default function DzikirLayout({
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, backgroundColor: "#E0F4F2" }}>
       <PagerView
-        style={{ flex: 1 }}
+        style={{ flex: 1, marginBottom: 32 }}
         initialPage={0}
         onPageSelected={(e) => setCurrentPage(e.nativeEvent.position)}
       >
         {dzikirList.map((item, index) => (
           <View key={index} style={{ padding: 16 }}>
-            <Text style={styles.subtitle}>{item.judul}</Text>
+            <Text
+              style={{ marginBottom: 10, fontWeight: "bold", fontSize: 16 }}
+            >
+              {item.judul}
+            </Text>
 
-            <View style={styles.iconRow}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginVertical: 10,
+              }}
+            >
               <TouchableOpacity>
                 <Icon name="volume-up" size={24} />
               </TouchableOpacity>
@@ -64,75 +73,65 @@ export default function DzikirLayout({
               </TouchableOpacity>
             </View>
 
-            <ScrollView style={styles.content}>
-              <Text style={[styles.arabic, { fontSize: fontSize + 6 }]}>
+            <ScrollView style={{ flex: 1 }}>
+              <Text
+                style={{ textAlign: "center", marginVertical: 8, fontSize }}
+              >
                 {item.arab}
               </Text>
               {showLatin && (
-                <Text style={[styles.text, { fontSize, fontStyle: "italic" }]}>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    marginVertical: 4,
+                    fontSize,
+                    fontStyle: "italic",
+                  }}
+                >
                   {item.latin}
                 </Text>
               )}
-              <Text style={[styles.text, { fontSize }]}>{item.arti}</Text>
+              <Text
+                style={{ textAlign: "center", marginVertical: 4, fontSize }}
+              >
+                {item.arti}
+              </Text>
             </ScrollView>
 
-            <View style={styles.counterContainer}>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 20,
+                justifyContent: "center",
+                marginBottom: 20
+              }}
+            >
               <TouchableOpacity
                 onPress={() => updateCounter(index, -counters[index])}
               >
-                <Icon name="refresh" size={24} />
+                <Icon name="refresh" size={30} />
               </TouchableOpacity>
-              <Text style={styles.counter}>{counters[index]}</Text>
+              <Text style={{fontSize: 20}}>{counters[index]}</Text>
               <TouchableOpacity onPress={() => updateCounter(index, 1)}>
-                <Icon name="add" size={24} />
+                <Icon name="add" size={30} />
               </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                alignItems: "center",
+                paddingVertical: 10,
+              }}
+            >
+              <Text
+                style={{
+                  color: "#555",
+                  fontWeight: "500",
+                }}
+              >{`${currentPage + 1} / ${dzikirList.length}`}</Text>
             </View>
           </View>
         ))}
       </PagerView>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          {`${currentPage + 1} / ${dzikirList.length}`}
-        </Text>
-      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, paddingBottom: 32, backgroundColor: "#E0F4F2" },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 16,
-  },
-  toggle: { color: "blue" },
-  title: { fontSize: 18, fontWeight: "bold" },
-  subtitle: { marginBottom: 10, fontWeight: "bold", fontSize: 16 },
-  iconRow: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginVertical: 10,
-  },
-  content: { flex: 1 },
-  arabic: { textAlign: "center", marginVertical: 8 },
-  text: { textAlign: "center", marginVertical: 4 },
-  counterContainer: {
-    flex: 1,
-    flexDirection: "row",
-    gap: 20,
-    justifyContent: "center",
-    marginTop: 10,
-  },
-  counter: { fontSize: 20, marginHorizontal: 10 },
-  footer: {
-    alignItems: "center",
-    paddingVertical: 10,
-  },
-  footerText: {
-    color: "#555",
-    fontWeight: "500",
-  },
-});
